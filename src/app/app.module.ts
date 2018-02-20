3298bc5206d0b57e1fd64226e16c659131e61150
Tamazlykar Daniil <tadani7@gmail.com>
2018-02-20 15:09:06 +0700

    Add and config firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { environment } from '../environments/environment';
 
 import { AppComponent } from './app.component';
-
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAKyHyWsVtSS7Bu2cRZEKVD0FdN2CLvXfE",
+    authDomain: "todo-list-2049b.firebaseapp.com",
+    databaseURL: "https://todo-list-2049b.firebaseio.com",
+    projectId: "todo-list-2049b",
+    storageBucket: "todo-list-2049b.appspot.com",
+    messagingSenderId: "818714320285"
+  }
 };
