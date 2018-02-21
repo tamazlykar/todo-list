9a9d5345bcb440a6bb1324686a446d9dafdff238
Tamazlykar Daniil <tadani7@gmail.com>
2018-02-21 10:27:36 +0700

    Add Firebase files for deploying

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAKyHyWsVtSS7Bu2cRZEKVD0FdN2CLvXfE",
+    authDomain: "todo-list-2049b.firebaseapp.com",
+    databaseURL: "https://todo-list-2049b.firebaseio.com",
+    projectId: "todo-list-2049b",
+    storageBucket: "todo-list-2049b.appspot.com",
+    messagingSenderId: "818714320285"
+  }
 };
